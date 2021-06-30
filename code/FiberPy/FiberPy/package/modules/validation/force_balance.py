88bdc7149bbafad77e392973908fb0a4df24f8ba
Sarah <65904084+Sarah-Kst@users.noreply.github.com>
2021-06-30 16:15:20 -0400

    Force-balance is working but needs refinement

---
+++ code/FiberPy/FiberPy/package/modules/validation/force_balance.py
@@ -0,0 +1,369 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 12 12:08:32 2021
+
+@author: sako231
+"""
+
+import os
+from natsort import natsorted
+import matplotlib.pyplot as plt
+
+from modules.half_sarcomere import half_sarcomere
+
+
+def compute_force_balance(dump_precision_list, dump_folder_list, output_folder) :
+    
+    max_thick_node_err = []
+    max_thin_node_err = []
+    
+    total_force_err = []
+    
+    for dump_folder in dump_folder_list: # Loop over each dump folder
+    
+        hs_file = []
+        
+        for filenames in os.listdir(dump_folder):
+            
+            filenames = os.path.join(dump_folder, filenames)
+            hs_file.append(filenames)
+        
+        hs_file = natsorted(hs_file) # sorting the dump files    
+        
+        hs = half_sarcomere.half_sarcomere(hs_file[-1]) # get last dump_file
+        
+        thick_err, thin_err = get_hs_thin_and_thick_errors(hs)
+        
+        max_thick_node_err.append(max(thick_err))
+        
+        max_thin_node_err.append(max(thin_err))
+        
+        total_force_err.append(check_total_force(hs))
+        
+
+    # PLOTS    
+    
+    output_image_file = os.path.join(output_folder, "Force_error.png")
+    
+    plt.figure()
+    plt.plot(dump_precision_list, total_force_err, color = "tab:red")
+    plt.xlabel("dump_precision")
+    plt.xticks(dump_precision_list)
+    plt.title("Total force error (%)")
+    plt.gca().spines['top'].set_visible(False)
+    plt.gca().spines['right'].set_visible(False)
+    plt.yscale("log")
+    
+    plt.savefig(output_image_file, dpi = 150)
+
+    output_image_file = os.path.join(output_folder, "Filaments_error.png")
+    
+    plt.figure()
+    plt.plot(dump_precision_list, max_thick_node_err, color = "tab:red", label = "max thick node error")
+    plt.plot(dump_precision_list, max_thin_node_err, color = "tab:blue", label = "max thin node error")
+    plt.xlabel("dump precision (# digits)")
+    plt.xticks(dump_precision_list)
+    plt.yscale("log")
+    plt.ylabel("[nm]")
+    plt.gca().spines['top'].set_visible(False)
+    plt.gca().spines['right'].set_visible(False)
+    plt.legend()
+    
+    plt.savefig(output_image_file, dpi = 150)
+
+def get_hs_thin_and_thick_errors(hs, show_error = False):
+     
+    thick_error = get_thick_node_error(hs)
+    thin_error = get_thin_node_error(hs)
+            
+    if show_error:
+        plt.figure()
+        plt.plot(thick_error, color = "tab:red", label = "thick error")
+        plt.plot(thin_error, color = "tab:blue", label = "thin error")
+        plt.xlabel("# fil")
+        plt.ylabel(" error (nm)")
+        #plt.yscale("log")
+        plt.legend()
+        
+    return thick_error, thin_error
+     
+
+def get_thick_node_error(hs):
+    """
+    Get the maximum node error for thick filaments
+    
+    """
+    
+    hs_length = hs["hs_data"]["hs_length"]
+    thick_node_err = []
+
+    for i, thick_fil in enumerate(hs["thick"]): # Loop over all thick filaments 
+    
+        
+        node_err = 0.0
+        
+        k_m = thick_fil["m_k_stiff"]
+        m_rl = thick_fil["m_inter_crown_rest_length"]
+        cbs_per_node = thick_fil["m_cbs_per_node"]
+        
+        # First node
+        
+        x_0 = thick_fil["cb_x"][0]
+        x_1 = thick_fil["cb_x"][0 + cbs_per_node]
+        
+        lamb = thick_fil["m_lambda"]
+        
+        residual = k_m * (x_0 - x_1 - m_rl) - k_m * (hs_length - lamb - x_0 - m_rl)
+        residual += get_m_cb_force(hs, thick_fil, 0, 0 + cbs_per_node)
+        residual += get_m_titin_force(hs, thick_fil, 0)
+        # residual += get_pc_force(hs, thick_fil, 0, 0 + cbs_per_node)
+        
+        node_err = max(node_err, abs(residual))
+
+        # Intermediate nodes
+        
+        for j in range(1, hs["hs_data"]["m_nodes_per_thick_filament"] - 1):
+            
+            x_0 = thick_fil["cb_x"][(j - 1) * cbs_per_node] # left node
+            x_1 = thick_fil["cb_x"][j * cbs_per_node]       # center node
+            x_2 = thick_fil["cb_x"][(j + 1) * cbs_per_node] # right node
+            
+            residual = k_m * (x_0 - x_1 - m_rl) - k_m * (x_1- x_2 - m_rl)
+            residual += get_m_cb_force(hs, thick_fil, j, j + cbs_per_node)
+            residual += get_m_titin_force(hs, thick_fil, j) 
+            
+            node_err = max(node_err, abs(residual))
+            
+        # Last node
+        
+        x_0 = thick_fil["cb_x"][(hs["hs_data"]["m_nodes_per_thick_filament"] - 2) * cbs_per_node] # second to last node
+        x_1 = thick_fil["cb_x"][-1] # last element
+        
+        residual = k_m * (x_0 - x_1 - m_rl)
+        residual += get_m_cb_force(hs, thick_fil, hs["hs_data"]["m_nodes_per_thick_filament"] - 2, hs["hs_data"]["m_nodes_per_thick_filament"] - 1)
+        residual += get_m_titin_force(hs, thick_fil, hs["hs_data"]["m_nodes_per_thick_filament"] - 1) 
+        
+        node_err = max(node_err, abs(residual)) 
+
+        thick_node_err.append(node_err) # store the max node error for each filament
+        
+    thick_node_err = [x/k_m for x in thick_node_err] # convert to nm
+        
+    return thick_node_err
+
+def get_thin_node_error(hs):
+    """
+    Get the maximum node error for thin filaments
+    
+    """
+
+    thin_node_err = []
+
+    for i, thin_fil in enumerate(hs["thin"]): # Loop over all thin filaments 
+    
+        nearest_thicks = get_nearest_thick_fil(hs, thin_fil)
+        
+        node_err = 0.0
+        
+        k_a = thin_fil["a_k_stiff"]
+        a_rl = thin_fil["a_inter_bs_rest_length"]
+        bs_per_node = thin_fil["a_bs_per_node"]
+        
+        # First node
+        
+        x_0 = thin_fil["bs_x"][0]
+        x_1 = thin_fil["bs_x"][0 + bs_per_node]
+                
+        residual = k_a * (x_1 - x_0 - a_rl)
+        residual -= get_a_cb_force(hs, thin_fil, 0, 0 + bs_per_node)
+        residual -= get_a_titin_force(hs, thin_fil, 0, nearest_thicks)
+        # residual -= get_pc_force(hs, thick_fil, 0, 0 + cbs_per_node)
+        
+        node_err = max(node_err, abs(residual)) 
+
+        # Intermediate nodes
+        
+        for j in range(1, hs["hs_data"]["a_nodes_per_thin_filament"] - 1):
+            
+            x_0 = thin_fil["bs_x"][(j - 1) * bs_per_node] # left node
+            x_1 = thin_fil["bs_x"][j * bs_per_node]       # center node
+            x_2 = thin_fil["bs_x"][(j + 1) * bs_per_node] # right node
+            
+            residual = k_a * (x_1 - x_0 - a_rl) - k_a * (x_2- x_1 - a_rl)
+            residual -= get_a_cb_force(hs, thin_fil, j, j + bs_per_node)
+            residual -= get_a_titin_force(hs, thin_fil, j, nearest_thicks)
+            
+            node_err = max(node_err, abs(residual)) 
+            
+        # Last node
+        
+        x_0 = thin_fil["bs_x"][(hs["hs_data"]["a_nodes_per_thin_filament"]  - 2) * bs_per_node] # second to last node
+        x_1 = thin_fil["bs_x"][(hs["hs_data"]["a_nodes_per_thin_filament"]  - 1) * bs_per_node] # last element
+        
+        residual = k_a * (x_1 - x_0 - a_rl)
+        residual -= get_a_cb_force(hs, thin_fil, hs["hs_data"]["a_nodes_per_thin_filament"] - 1, hs["hs_data"]["a_nodes_per_thin_filament"])
+        residual -= get_a_titin_force(hs, thin_fil, hs["hs_data"]["a_nodes_per_thin_filament"]- 1, nearest_thicks)
+            
+        node_err = max(node_err, abs(residual))
+        
+        thin_node_err.append(node_err)  # store the max node error for each filament
+        
+    thin_node_err = [x/k_a for x in thin_node_err] # convert to nm
+        
+    return thin_node_err
+           
+
+def get_m_cb_force(hs, thick_fil, idx_1, idx_2):
+    """
+    Calculate bound cbs force contribution to thick node forces
+    """
+    
+    cb_force = 0.0
+    
+    for cb_idx in range(idx_1, idx_2):
+        
+        if thick_fil["cb_bound_to_a_f"][cb_idx] >= 0:
+            
+          cb_state = thick_fil["cb_state"][cb_idx]
+          cb_extension = hs["hs_data"]["cb_extensions"][cb_state - 1]
+          
+          thin_fil_id = thick_fil["cb_bound_to_a_f"][cb_idx]
+          thin_fil = hs["thin"][thin_fil_id]
+          
+          bs_idx = thick_fil["cb_bound_to_a_n"][cb_idx]
+          bs_x = thin_fil["bs_x"][bs_idx]
+          
+          cb_x = thick_fil["cb_x"][cb_idx]
+    
+          delta_x = cb_x + cb_extension - bs_x
+    
+          cb_force += thick_fil["m_k_cb"] * delta_x
+  
+    return cb_force
+
+def get_a_cb_force(hs, thin_fil, idx_1, idx_2):
+    """
+    Calculate bound cbs force contribution to thick node forces
+    """
+    cb_force = 0.0
+      
+    for bs_idx in range(idx_1, idx_2):
+        
+      if thin_fil["bound_to_m_f"][bs_idx] >= 0:
+          
+        thick_fil_id = thin_fil["bound_to_m_f"][bs_idx]
+        thick_fil = hs["thick"][thick_fil_id]
+        
+        cb_idx = thin_fil["bound_to_m_n"][bs_idx]
+        cb_x = thick_fil["cb_x"][cb_idx]
+        
+        cb_state = cb_state = thick_fil["cb_state"][cb_idx]
+        cb_extension = hs["hs_data"]["cb_extensions"][cb_state - 1]
+        
+        bs_x = thin_fil["bs_x"][bs_idx]
+        
+        delta_x = cb_x + cb_extension - bs_x
+  
+        cb_force += thick_fil["m_k_cb"] * delta_x
+    
+      return cb_force
+  
+    
+def get_a_titin_force(hs, thin_fil, bs_node, nearest_thicks, bs_per_node=2, cb_per_node=6):
+      """
+      Returns titin contribution to thin node force
+    
+      """
+      titin_force = 0
+      
+      if bs_node == hs["titin"]["t_attach_a_node"] - 1:
+          
+          bs_idx = bs_node * bs_per_node 
+          bs_x = thin_fil["bs_x"][bs_idx]
+          
+          cb_idx = cb_per_node * ( hs["titin"]["t_attach_m_node"] - 1 )
+    
+          for thick_fil_id in nearest_thicks:
+              
+            thick_fil = hs["thick"][thick_fil_id]
+            
+            cb_x = thick_fil["cb_x"][cb_idx]
+
+            delta_x = cb_x - bs_x 
+            
+            titin_force += hs["titin"]["t_k_stiff"] * (delta_x - hs["titin"]["t_slack_length"])
+    
+      return titin_force
+
+def get_m_titin_force(hs, thick_fil, cb_node, bs_per_node=2, cb_per_node=6):
+      """
+      Returns titin contribution to thick node force
+    
+      """
+      titin_force = 0
+      
+      if cb_node == hs["titin"]["t_attach_m_node"] - 1:
+          
+          cb_idx = cb_node * cb_per_node 
+          cb_x = thick_fil["cb_x"][cb_idx]
+          
+          bs_idx = bs_per_node * (hs["titin"]["t_attach_a_node"] - 1)
+
+          for thin_id in thick_fil["nearest_actin_filaments"]:
+              
+            thin_fil = hs["thin"][thin_id]
+            
+            bs_x = thin_fil["bs_x"][bs_idx]
+            
+            delta_x = cb_x - bs_x 
+            
+            titin_force += hs["titin"]["t_k_stiff"] * (delta_x - hs["titin"]["t_slack_length"])
+    
+      return titin_force
+  
+def get_nearest_thick_fil(hs, thin_fil):
+    """
+    Returns array of the 3 nearest thick filaments  
+    
+    """
+    nearest_thicks = []
+    
+    for thick_fil in hs["thick"]:
+        
+      if thin_fil["thin_id"] in thick_fil["nearest_actin_filaments"]:
+          
+          nearest_thicks.append(thick_fil["thick_id"])
+
+    return nearest_thicks
+
+def check_total_force(hs):
+    """
+    Compare the half-sarcomere force to the 
+    re-calculated total force from hs dump file
+    """
+    print("myosin filament density is ", 0.25e15)
+    
+    m_filament_density = 0.25e15
+    
+    hs_force = hs["hs_data"]["hs_force"]
+    hs_length = hs["hs_data"]["hs_length"]
+    
+    computed_force = 0.0
+    
+    for thick_fil in hs["thick"]:
+        
+        k_m = thick_fil["m_k_stiff"]       
+        m_rl = thick_fil["m_inter_crown_rest_length"]          
+        lamb = thick_fil["m_lambda"]
+        
+        cb_x_m_line = thick_fil["cb_x"][0]
+        
+        computed_force += k_m * (hs_length - lamb - m_rl - cb_x_m_line)
+        
+    computed_force /= len(hs["thick"])     # average over all thick filaments
+    computed_force *= m_filament_density * 1e-9 # force in N/m²
+    
+    err_force = (hs_force - computed_force)/hs_force * 100
+    
+    return abs(err_force)
+                                                
--- code/FiberPy/FiberPy/package/modules/validation/validation.py
+++ code/FiberPy/FiberPy/package/modules/validation/validation.py
@@ -10,6 +10,7 @@ sys.path.append(MODULES_ROOT)
 from modules.validation import myosin_kinetics
 from modules.validation import mybpc_kinetics
 from modules.validation import actin_kinetics
+from modules.validation import force_balance
 
 
 def run_validation(kinetic_data, batch_file_string):
@@ -17,75 +18,126 @@ def run_validation(kinetic_data, batch_file_string):
     
     print("Run_validation starting")
     
-    # Get the model/option/protocol file and output folder 
-    
     base_folder = os.path.dirname(batch_file_string)
-    
-    if (kinetic_data['relative_to'] == 'this_file'):
         
-        model_file = os.path.join(base_folder, kinetic_data['model_file'])
-        
-        protocol_file = os.path.join(base_folder, kinetic_data['protocol_file'])
-        
-        options_file = os.path.join(base_folder, kinetic_data['options_file'])
-        
-        output_folder = os.path.join(base_folder, kinetic_data['output_data_folder'])
-        
-    else:
-        
-        model_file = kinetic_data['model_file']
-        protocol_file = kinetic_data['protocol_file']
-        options_file = kinetic_data['options_file']
-        output_folder = kinetic_data['output_data_folder']
-        
-    # Creatte output folder if it does not exist
-
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)    
-        
-    # Get dump_folder from option file
-    
-    with open(options_file, 'r') as f:
-        opt = json.load(f)
-    
-    base_folder = os.path.dirname(options_file)
-    
-    if 'status_files' in opt['options']:
-        if opt['options']['status_files']['relative_to'] == 'this_file':
-            dump_folder = os.path.join(base_folder, opt['options']['status_files']['status_folder'])
-        else:
-            dump_folder = opt['options']['status_files']['status_folder']
-            
-    else:
-        raise RuntimeError("No dump folder found to run the test")
-        
-    # Get number of adjacent bs from option file
-    
-    if 'adjacent_bs' in opt['options']:
-        adj_bs = opt["options"]["adjacent_bs"]        
-    else:
-        adj_bs = 0
-    
-    # Now run the kinetics test or force-balance test
+    # Get  model/protocol/option files depending on the validation type
     
     val_type = kinetic_data["validation_type"]
     
-    if (val_type == 'm_kinetics'): 
+    if (val_type == 'force_balance'):  # option files is a list for force-balance check
+    
+        if isinstance(kinetic_data['options_file'], list): # check if a list of options is provided        
+            
+            dump_folder_list = []
+            dump_precision_list = []
         
-        print("Myosin kinetics check")
-        
-        myosin_kinetics.compute_rate(model_file, protocol_file, dump_folder, output_folder, adj_bs)
-        
-    if (val_type == 'c_kinetics'): 
-              
-        print("Mybpc kinetics check")
-        
-        mybpc_kinetics.compute_rate(model_file, protocol_file, dump_folder, output_folder, adj_bs)
-        
-    if (val_type == 'a_kinetics'): 
+            for elmt in kinetic_data['options_file']:
                 
-        print("Actin kinetics check")
+                print("LOOK HERE", elmt)
+                
+                if (kinetic_data['relative_to'] == 'this_file'):
+                
+                    options_file = os.path.join(base_folder, elmt)
+                    output_folder = os.path.join(base_folder, kinetic_data['output_data_folder'])
+                    
+                else:
+                    
+                    options_file = elmt
+                    output_folder = kinetic_data['output_data_folder']
+                    
+                with open(options_file, 'r') as f:
+                    opt = json.load(f)
+                    
+                option_dir = os.path.dirname(options_file)
+                    
+                if 'status_files' in opt['options']:
+                    
+                    if opt['options']['status_files']['relative_to'] == 'this_file':
+                        dump_folder = os.path.join(option_dir , opt['options']['status_files']['status_folder'])
+                    else:
+                        dump_folder = opt['options']['status_files']['status_folder']
+                                    
+                else:
+                    raise RuntimeError("No dump folder found to run the test")
+                    
+                dump_precision_list.append(opt["options"]["dump_precision"])
+                dump_folder_list.append(dump_folder)
+                
+        else:
+            raise RuntimeError("Option file(s) not provided in a list form")
+                                    
+        print("force_balance check")
         
-        actin_kinetics.compute_rate(model_file, protocol_file, dump_folder, output_folder)   
+        force_balance.compute_force_balance(dump_precision_list, dump_folder_list, output_folder) 
+        
+    else:  # it is a kinetic test    
+    
+        if (kinetic_data['relative_to'] == 'this_file'):  
+            
+            model_file = os.path.join(base_folder, kinetic_data['model_file'])
+        
+            protocol_file = os.path.join(base_folder, kinetic_data['protocol_file'])
+            
+            options_file = os.path.join(base_folder, kinetic_data['options_file'])
+            
+            output_folder = os.path.join(base_folder, kinetic_data['output_data_folder'])
+            
+        else:            
+            model_file = kinetic_data['model_file']
+            protocol_file = kinetic_data['protocol_file']
+            options_file = kinetic_data['options_file']
+            output_folder = kinetic_data['output_data_folder']
+            
+        # Create output folder if it does not exist
+
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)   
+                                       
+        # Get dump_folder from option file
+        
+        with open(options_file, 'r') as f:
+            opt = json.load(f)
+        
+        option_dir = os.path.dirname(options_file)
+                
+        if 'status_files' in opt['options']:
+            if opt['options']['status_files']['relative_to'] == 'this_file':
+                dump_folder = os.path.join(option_dir, opt['options']['status_files']['status_folder'])
+            else:
+                dump_folder = opt['options']['status_files']['status_folder']
+                
+        else:
+            raise RuntimeError("No dump folder found to run the test")
+            
+        # Get number of adjacent bs from option file
+        
+        if 'adjacent_bs' in opt['options']:
+            adj_bs = opt["options"]["adjacent_bs"]        
+        else:
+            adj_bs = 0
+        
+        # Now run the kinetics test
+        
+        val_type = kinetic_data["validation_type"]
+        
+        if (val_type == 'm_kinetics'): 
+            
+            print("Myosin kinetics check")
+            
+            myosin_kinetics.compute_rate(model_file, protocol_file, dump_folder, output_folder, adj_bs)
+            
+        if (val_type == 'c_kinetics'): 
+                  
+            print("Mybpc kinetics check")
+            
+            mybpc_kinetics.compute_rate(model_file, protocol_file, dump_folder, output_folder, adj_bs)
+            
+        if (val_type == 'a_kinetics'): 
+                    
+            print("Actin kinetics check")
+            
+            actin_kinetics.compute_rate(model_file, protocol_file, dump_folder, output_folder)   
+            
+
         
         
