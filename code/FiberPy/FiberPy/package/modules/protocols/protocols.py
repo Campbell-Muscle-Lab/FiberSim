eee1d1e994e89dbf3d24fc5da017ab7f079a0f5e
Sarah <65904084+Sarah-Kst@users.noreply.github.com>
2022-04-01 17:37:03 -0400

    Preparing website tab for Ochala's manuscript

--- code/FiberPy/FiberPy/package/modules/protocols/protocols.py
+++ code/FiberPy/FiberPy/package/modules/protocols/protocols.py
@@ -28,7 +28,7 @@ def write_protocol_to_file(prot, prot_file_string):
 
 def create_length_control_protocol(time_step=0.0001, n_points=1000,
                               initial_pCa=9.0, step_pCa=4.5,
-                              step_pCa_s=0.05,
+                              step_pCa_s=0.10,
                               mode_vector = [],
                               delta_hsl=[]):
     """ Creates a length control protocol as a Pandas dataframe """
