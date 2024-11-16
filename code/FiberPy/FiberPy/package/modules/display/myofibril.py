508b4f8bc7b933c3a3616461a6202e9ebce23039
Ken Campbell <8246018+kenatcampbellmusclelab@users.noreply.github.com>
2024-11-16 14:18:19 -0500

    Update myofibril.py

--- code/FiberPy/FiberPy/package/modules/display/myofibril.py
+++ code/FiberPy/FiberPy/package/modules/display/myofibril.py
@@ -32,14 +32,19 @@ def draw_myofibril_frame(df, frame=1, image_file_string=[]):
     ax_myofibril = 0
     
     z_width = 100
+    z_line_color = 'black'
     
     m_width = 50
+    m_line_color = 'darkviolet'
     
-    thin_y_pos = 0.8
+    thin_y_pos = 0.9
     thin_width = 0.1
+    thin_color = 'salmon'
     
-    thick_y_pos = 0.4
+    thick_y_pos = 0.5
     thick_width = 0.3
+    thick_color = 'turquoise'
+    
         
     # Work out how many half-sarcomeres there are
     command_length_cols = [x for x in d.columns.to_list() if x.endswith('command_length')]
@@ -59,8 +64,7 @@ def draw_myofibril_frame(df, frame=1, image_file_string=[]):
     # Plot half_sarcomere
     x_anchor = 0
     
-    # for i in range(no_of_half_sarcomeres):
-    for i in range(4):
+    for i in range(no_of_half_sarcomeres):
         
         hsl_string = 'hs_%i_length' % (i+1)
         thin_length_string = 'hs_%i_a_length' % (i+1)
@@ -78,27 +82,28 @@ def draw_myofibril_frame(df, frame=1, image_file_string=[]):
             hs_polarity = -1
 
         # Z line
-        ax[ax_myofibril].add_patch(Rectangle((x_anchor - 0.5* z_width, 0), z_width, 1, color='k'))
+        ax[ax_myofibril].add_patch(Rectangle((x_anchor - 0.5* z_width, 0), z_width, 1,
+                                             color = z_line_color))
 
         # M line
         ax[ax_myofibril].add_patch(Rectangle((x_anchor + hs_polarity*hs_length - 0.5*m_width, 0.1),
-                                            m_width, 0.8, color='g'))
+                                            m_width, 0.8, color = m_line_color))
         
         # Thin filament
-        ax[ax_myofibril].add_patch(Rectangle((x_anchor, thin_y_pos - hs_polarity*thin_width),
+        ax[ax_myofibril].add_patch(Rectangle((x_anchor, thin_y_pos - 0.5*thin_width),
                                               hs_polarity * thin_length,
-                                               thin_width, color='r'))
+                                               thin_width, color = thin_color))
         
         # Thick filament
-        ax[ax_myofibril].add_patch(Rectangle((x_anchor + hs_polarity*hs_length, thick_y_pos),
+        ax[ax_myofibril].add_patch(Rectangle((x_anchor + hs_polarity*hs_length, thick_y_pos - 0.5*thick_width),
                                              hs_polarity * thick_length,
-                                             thick_width, color = 'b'))
+                                             thick_width, color = thick_color))
 
         # Move the anchor
         if (hs_polarity == 1):        
             x_anchor = x_anchor + hs_length
         
-    plt.xlim(0, 20000)
+    plt.xlim(0, 70000)
     plt.ylim(-1, 2)
     
     
@@ -132,15 +137,15 @@ def create_movie_from_image_folder(image_folder, movie_file_string):
     
     
 if __name__ == "__main__":
-    fs = 'c:/ken/github/campbellmusclelab/publications/paper_fibersim_sequential_relaxation/simulations/kens_playground/f/sim_data/sim_output/1/sim_pCa_59_s_0_r1.txt'
+    fs = 'd:/ken/github/campbellmusclelab/publications/paper_fibersim_sequential_relaxation/simulations/kens_playground/f/sim_data/sim_output/1/sim_pCa_59_s_0_r1.txt'
     
     d = pd.read_csv(fs, sep='\t')
     
-    od = 'c:/temp/test_images/ken'
-    od2 = 'c:/temp/test_images'
-    mfs = 'c:/temp/myofibril.mp4'
+    od = 'd:/temp/test_images/ken'
+    od2 = 'd:/temp/test_images'
+    mfs = 'd:/temp/myofibril.mp4'
     
-    for i in np.arange(1, 4500, 10000):
+    for i in np.arange(1, 4500, 10):
         draw_myofibril_frame(d, i, od)
     
     create_movie_from_image_folder(od2, mfs)
