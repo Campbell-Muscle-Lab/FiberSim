733d801280108c0038808cccffb9ecf59ceeae86
Ken Campbell <8246018+kenatcampbellmusclelab@users.noreply.github.com>
2025-09-30 13:24:18 -0400

    Biophys abstract

--- demo_files/isotonic/twitch/MATLAB_code/fv_curves.m
+++ demo_files/isotonic/twitch/MATLAB_code/fv_curves.m
@@ -5,16 +5,20 @@ data_folders = ["../sim_data/release", ...
     "../sim_data/afterload"];
 
 t_ticks = [0 0.25];
-fit_t = [0.175 0.21 ; 0.13 0.17];
+fit_t = [0.175 0.21 ; 0.175 0.21];
 
 panels_wide = 2;
+panels_high = 7;
 
 % Make a figure
 sp = layout_subplots( ...
         panels_wide = panels_wide, ...
-        panels_high = 3, ...
+        panels_high = panels_high, ...
         figure_width = 7, ...
-        x_to_y_ratio = 3);
+        x_to_y_ratio = 4);
+
+% Get the color_map
+cm = return_matplotlib_default_colors();
 
 % Loop through folders
 for fold_i = 1 : numel(data_folders)
@@ -33,11 +37,13 @@ for fold_i = 1 : numel(data_folders)
 
         subplot(sp(fold_i));
         hold on;
-        plot(d.time(ti), d.hs_1_force(ti), '-');
-
-        ylim([0 110000])
+        plot(d.time(ti), 10.^(-d.hs_1_pCa(ti)), '-');
 
         subplot(sp(fold_i + panels_wide));
+        hold on;
+        plot(d.time(ti), d.hs_1_force(ti), '-');
+
+        subplot(sp(fold_i + 2*panels_wide));
         hold on;
         plot(d.time(ti), d.hs_1_length(ti), '-');
 
@@ -52,18 +58,46 @@ for fold_i = 1 : numel(data_folders)
 
         plot(fit_line.x_fit, fit_line.y_fit, 'k-');
 
-        ylim([1040 1100]);
+        % Thin
+        subplot(sp(fold_i + (3*panels_wide)));
+        for pop_i = 1 : 2
+            pop_string = sprintf('hs_1_a_pop_%i', pop_i);
+            plot(d.time(ti), d.(pop_string)(ti), '-', ...
+                Color = cm(pop_i,:));
+        end
+
+        % Thick
+        subplot(sp(fold_i + (4*panels_wide)));
+        for pop_i = 1 : 4
+            pop_string = sprintf('hs_1_m_pop_%i', pop_i);
+            plot(d.time(ti), d.(pop_string)(ti), '-', ...
+                Color = cm(pop_i,:));
+        end
+
+        % Mybpc
+        subplot(sp(fold_i + (5*panels_wide)));
+        for pop_i = 1 : 3
+            pop_string = sprintf('hs_1_c_pop_%i', pop_i);
+            plot(d.time(ti), d.(pop_string)(ti), '-', ...
+                Color = cm(pop_i,:));
+        end
+
     end
 end
 
+for i = 1 : 2
 
+    vi = find(v(i,:) > 0);
 
-subplot(sp(5));
-hold on;
-plot(f(1, :), v(1,:), 'ro-');
-plot(f(2,:), v(2,:), 'bs-')
+    syms = {'o-', 's-'};
 
-subplot(sp(6));
-hold on;
-plot(f(1,:), f(1,:).*v(1,:), 'r-o');
-plot(f(2,:), f(2,:).*v(2,:), 'bs-');
+    subplot(sp((panels_high-1)*panels_wide + 1));
+    hold on;
+    plot(f(i, vi), v(i,vi), syms{i}, ...
+        Color = cm(i,:));
+
+    subplot(sp((panels_high-1)*panels_wide + 2));
+    hold on;
+    plot(f(i, vi), f(i, vi).*v(i, vi), syms{i}, ...
+        Color = cm(i,:));
+end
